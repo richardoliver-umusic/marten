b8d321b1e3b9a8aea74120212436d24bff8ca35a
Richard Oliver <richard.oliver@umusic.com>
2019-02-28 14:25:27 +0000

    Add missing semicolons when dropping system functions

--- src/Marten/Events/AppendEventFunction.cs
+++ src/Marten/Events/AppendEventFunction.cs
@@ -31,19 +31,19 @@ namespace Marten.Events
             writer.WriteLine($@"
 CREATE OR REPLACE FUNCTION {Identifier}(stream {streamIdType}, stream_type varchar, tenantid varchar, event_ids uuid[], event_types varchar[], dotnet_types varchar[], bodies jsonb[]) RETURNS int[] AS $$
 DECLARE
-	event_version int;
-	event_type varchar;
-	event_id uuid;
-	body jsonb;
-	index int;
-	seq int;
+    event_version int;
+    event_type varchar;
+    event_id uuid;
+    body jsonb;
+    index int;
+    seq int;
     actual_tenant varchar;
-	return_value int[];
+    return_value int[];
 BEGIN
-	select version into event_version from {databaseSchema}.mt_streams where {streamsWhere};
-	if event_version IS NULL then
-		event_version = 0;
-		insert into {databaseSchema}.mt_streams (id, type, version, timestamp, tenant_id) values (stream, stream_type, 0, now(), tenantid);
+    select version into event_version from {databaseSchema}.mt_streams where {streamsWhere};
+    if event_version IS NULL then
+        event_version = 0;
+        insert into {databaseSchema}.mt_streams (id, type, version, timestamp, tenant_id) values (stream, stream_type, 0, now(), tenantid);
     else
         if tenantid IS NOT NULL then
             select tenant_id into actual_tenant from {databaseSchema}.mt_streams where {streamsWhere};
@@ -51,31 +51,31 @@ BEGIN
                 RAISE EXCEPTION 'Marten: The tenantid does not match the existing stream';
             end if;
         end if;
-	end if;
+    end if;
 
-	index := 1;
-	return_value := ARRAY[event_version + array_length(event_ids, 1)];
+    index := 1;
+    return_value := ARRAY[event_version + array_length(event_ids, 1)];
 
-	foreach event_id in ARRAY event_ids
-	loop
-	    seq := nextval('{databaseSchema}.mt_events_sequence');
-		return_value := array_append(return_value, seq);
+    foreach event_id in ARRAY event_ids
+    loop
+        seq := nextval('{databaseSchema}.mt_events_sequence');
+        return_value := array_append(return_value, seq);
 
-	    event_version := event_version + 1;
-		event_type = event_types[index];
-		body = bodies[index];
+        event_version := event_version + 1;
+        event_type = event_types[index];
+        body = bodies[index];
 
-		insert into {databaseSchema}.mt_events
-			(seq_id, id, stream_id, version, data, type, tenant_id, {DocumentMapping.DotNetTypeColumn})
-		values
-			(seq, event_id, stream, event_version, body, event_type, tenantid, dotnet_types[index]);
+        insert into {databaseSchema}.mt_events
+            (seq_id, id, stream_id, version, data, type, tenant_id, {DocumentMapping.DotNetTypeColumn})
+        values
+            (seq, event_id, stream, event_version, body, event_type, tenantid, dotnet_types[index]);
 
-		index := index + 1;
-	end loop;
+        index := index + 1;
+    end loop;
 
-	update {databaseSchema}.mt_streams set version = event_version, timestamp = now() where {streamsWhere};
+    update {databaseSchema}.mt_streams set version = event_version, timestamp = now() where {streamsWhere};
 
-	return return_value;
+    return return_value;
 END
 $$ LANGUAGE plpgsql;
 ");
@@ -84,7 +84,7 @@ $$ LANGUAGE plpgsql;
         protected override string toDropSql()
         {
             var streamIdType = _events.GetStreamIdDBType();
-            return $"drop function if exists {Identifier} ({streamIdType}, varchar, varchar, uuid[], varchar[], jsonb[])";
+            return $"drop function if exists {Identifier} ({streamIdType}, varchar, varchar, uuid[], varchar[], jsonb[]);";
         }
     }
 
